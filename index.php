17fa6594436f2a87d78b606b786c83ca44eb53bb
samuelepietrasanta <samuele.pietrasanta@gmail.com>
2021-10-15 17:46:47 +0200

    esercizio finito

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="style.css">
     <title>PHP Dischi</title>
 </head>
 <body>
@@ -18,9 +19,14 @@ require(__DIR__ . '/database.php');
     <div class="container">
         <div class="row">
             <?php foreach($databaseDischi as $disco){; ?>
-                <div class="col-4">
-                    <h2 class="text-danger"><?php echo $disco['title']; ?></h2>
-                    <h4><?php echo $disco['author']; ?></h4>
+                <div class="col-3 disco text-center">
+                    <img <?php echo 'src=' . $disco['poster']; ?> alt="immagine">
+                    <h4><?php echo $disco['title']; ?></h4>
+                    <h6><?php echo $disco['author']; ?></h6>
+                    <div>
+                        <p><?php echo$disco['genre'] . ' ' . $disco['year']; ?>
+                        </p>
+                    </div>
                 </div>
             <?php }; ?>
         </div>
