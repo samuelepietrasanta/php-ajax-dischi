d19334379c38a0d51d66d037394e87154a16fa67
samuelepietrasanta <samuele.pietrasanta@gmail.com>
2021-10-15 17:34:02 +0200

    aggiunto bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"> 
     <title>PHP Dischi</title>
 </head>
 <body>
@@ -12,12 +13,22 @@
 require(__DIR__ . '/database.php'); 
 ?>
 
-<?php foreach($databaseDischi as $disco){; ?>
 
-    <h1><?php echo $disco['title']; ?>
-    </h1>
 
-<?php }; ?>
+    <div class="container">
+        <div class="row">
+            <?php foreach($databaseDischi as $disco){; ?>
+                <div class="col-4">
+                    <h2 class="text-danger"><?php echo $disco['title']; ?></h2>
+                    <h4><?php echo $disco['author']; ?></h4>
+                </div>
+            <?php }; ?>
+        </div>
+    </div>
+
+    
+
+
 
 
     
